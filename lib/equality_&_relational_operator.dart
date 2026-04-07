ccd30c57ebb2294ac13e692d2f5f3b78e3383cf6
soekhant01 <soekhant.dev@gmail.com>
2026-04-07 11:16:04 +0630

    day2(equality and relational operator)

---
+++ lib/equality_&_relational_operator.dart
@@ -0,0 +1,18 @@
+void main(){
+  String name = "soe khant";
+  int a = 10;
+  int b = 20;
+  double c = 2.5;
+
+  print(a == b);
+  print(name == "soe khant");
+
+  print(a != b);
+
+  print(b < a);
+  print(b > a);
+
+  print(a >= 15);
+  print(a <= 15);
+
+}
