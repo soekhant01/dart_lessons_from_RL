203d3cac2c5d537c111c5c42931f426181afad09
soekhant01 <soekhant.dev@gmail.com>
2026-04-07 10:42:36 +0630

    day2(arithmetic operator)

---
+++ lib/arithmetic_operator.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  int a = 10;
+  int b = 20;
+  double c = 2.5;
+
+  print(a+b);
+  print(a+c);
+  print(b-c);
+  print(-a+b); // -expr(-a) //Unary minus,
+
+  print(a * b* c);
+
+  print(b / a);
+
+  print("it will return integer value ${b ~/ a}"); // Divide, returning an integer result
+
+}
