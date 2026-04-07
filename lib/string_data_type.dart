8c8dd85460bc177f8e6946e2b71ce21d033ffeeb
soekhant01 <soekhant.dev@gmail.com>
2026-04-07 11:29:05 +0630

    day2(string data type)

---
+++ lib/string_data_type.dart
@@ -0,0 +1,13 @@
+void main(){
+  String stringValue = " ha ha ";
+  String name = 'soe khant';
+  String alphabet = 'Abc';
+
+
+  print(name.length);
+  print(stringValue.length);
+  print(stringValue.trim().length);
+  print(alphabet.codeUnits); // it will return ascii codes with an array
+  print(name.toLowerCase());
+  print(name.toUpperCase());
+}
